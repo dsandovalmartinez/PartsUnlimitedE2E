206fc557cdbbe8524cca2d7b1797950684ba7d3f
Daniel Sandoval <d.sandoval.martinez@accenture.com>
2020-06-16 23:56:25 -0500

    Just added another comment

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // Primary key
+        // Primary key, bro
         [Key]
         public int CartItemId { get; set; }
 
